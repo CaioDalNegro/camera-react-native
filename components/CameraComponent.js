84f786ed55a1bc63394ebc47cc717b086bd01cbc
CaioDalNegro <caiodalnegro@gmail.com>
2025-09-26 19:06:04 -0300

    NEW: criação do componente

---
+++ components/CameraComponent.js
@@ -0,0 +1,15 @@
+// Componente camera
+// Importações dos hookes useState, useEffect e useRef
+import React, { useState, useEffect, useRef } from "react";
+
+// Importação dos componentes React Native que serão utilizados na tela
+import { Button, StyleSheet, Text, TouchableOpacity, View, Image } from "react-native";
+
+// Importação do Expo-Câmera
+import { CameraView, useCameraPermissions } from "expo-camera";
+
+function CameraComponent(){
+
+}
+
+export default CameraComponent;
